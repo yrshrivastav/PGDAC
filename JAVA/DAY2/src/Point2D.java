af628b1956e3d0bfb3380609a45d952bf8b6a19a
yrshrivastav <yrshrivastav42+github@gmail.com>
2025-09-02 18:47:19 +0530

    done q1
    done q1

--- JAVA/DAY2/src/Point2D.java
+++ JAVA/DAY2/src/Point2D.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Point2D {
     /*
      * 
@@ -9,6 +10,25 @@ public class Point2D {
             4.2 Add  show() method  --to return point's x & y co-ords
 
      */
-    public static void main(String main)
+        private int x;
+        private int y;
+
+        Point2D(int x, int y){
+            this.x=x;
+            this.y=y;
+            System.out.println("Parameterized constructor called");
+        }
+        public String show(){
+            System.out.println("Show method called by instance of Point2D class");
+            return "Point X : "+this.x +" Point Y : "+this.y;
+            
+        }
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter coordinates X and Y");
+            Point2D p = new Point2D(sc.nextInt(), sc.nextInt());
+            System.out.println(p.show());
+        }
     
 }
