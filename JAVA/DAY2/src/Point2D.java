6114d61264631f6fa2927b60f7e1a0934613e854
yrshrivastav <yrshrivastav42+github@gmail.com>
2025-09-02 17:38:07 +0530

    nothing imp

---
+++ JAVA/DAY2/src/Point2D.java
@@ -0,0 +1,14 @@
+public class Point2D {
+    /*
+     * 
+            Create a  class Point2D   for representing a point in x & y(int) co-ordinate system.
+            (Tight encapsulation please !)
+
+            4.1 Create a parameterized constructor to accept x & y co-ords.
+
+            4.2 Add  show() method  --to return point's x & y co-ords
+
+     */
+    public static void main(String main)
+    
+}
