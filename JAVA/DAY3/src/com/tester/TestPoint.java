0c093bdd6871b7fde4cf9c5ddd0a0555ef996425
yrshrivastav <yrshrivastav42+github@gmail.com>
2025-09-04 16:06:37 +0530

    done

--- JAVA/DAY3/src/com/tester/TestPoint.java
+++ JAVA/DAY3/src/com/tester/TestPoint.java
@@ -1,10 +1,16 @@
 package com.tester;
-
 import java.util.Scanner;
 
 public class TestPoint {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter coordinates X and Y for two points");
-    
-
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Coordinates of X and Y");
+        Point2D p1 = new Point2D(sc.nextInt(), sc.nextInt());
+        Point2D p2 = new Point2D(sc.nextInt(), sc.nextInt());
+        System.out.println(p1.show(p2));
+        System.out.println("Both Points are equal : "+p1.isEqual());
+        System.out.println("Both Points are equal : "+p2.isEqual());
+        p1.calculateDistance(p2);
+        sc.close();
+    }
 }
